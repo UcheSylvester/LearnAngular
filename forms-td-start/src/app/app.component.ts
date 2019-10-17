3a7c699bffe0eee0264cbcaf17dafe04bd4845df
UcheSylvester <okorocode@gmail.com>
2019-10-17 09:47:21 +0100

    learning forms

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  @ViewChild("form") signupForm: NgForm;
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log("submitted");
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
